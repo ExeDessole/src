a50d4105d56aa91b2048c7252ad19054d96d8f63
Exe Dessole <dessolem@yahoo.com>
2025-02-03 02:14:59 -0300

    Nuevo inicio

---
+++ server.js
@@ -0,0 +1,15 @@
+//Configuración de Express
+import express from 'express'
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req,res) =>{
+    res.send('Pagina principal')
+})
+
+//Configuración del SERVER
+const SERVER = app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en PUERTO: ${PORT}`)
+})
+
+
